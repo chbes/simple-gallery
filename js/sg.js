5b634b36573d691038dca369fe33fea3bd9e594c
BES <charles.bes@capgemini.com>
2016-10-28 17:00:03 +0200

    click image

--- js/sg.js
+++ js/sg.js
@@ -1,12 +1,14 @@
 $(function() {
 
-    $('#sg1').click( function(event) {
-        console.log('1');
+    $('#sg1 img').click( function() {
+        console.log('modal show');
+        var srcImg = $(this).attr('src');
+        $('#modal1 img').attr('src', srcImg);
         $('#modal1').show();
     });
 
     $('#modal1').click( function(event) {
-        console.log('2');
+        console.log('modal hide');
         $('#modal1').hide();
     });
 
