19e8ff46c60fb4c018396aeb73587053c83edeb1
chbes <bes.charles@gmail.com>
2016-10-30 20:00:57 +0100

    add css class - flou

--- js/sg.js
+++ js/sg.js
@@ -4,11 +4,13 @@ $(function() {
         console.log('modal show');
         var srcImg = $(this).attr('src');
         $('#modal1 img').attr('src', srcImg);
+        $('html').addClass('flou');
         $('#modal1').show();
     });
 
     $('#modal1').click( function(event) {
         console.log('modal hide');
+        $('html').removeClass('flou');
         $('#modal1').hide();
     });
 
