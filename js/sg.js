28d90a9f844d8a5cebc6d19de22813a6f51babee
chbes <bes.charles@gmail.com>
2016-10-31 15:35:08 +0100

    refactor modal

--- js/sg.js
+++ js/sg.js
@@ -3,14 +3,16 @@ $(function() {
     $('#sg1 img').click( function() {
         console.log('modal show');
         var srcImg = $(this).attr('src');
-        $('#modal1 img').attr('src', srcImg);
-        $('html').addClass('flou');
+        $('#modal1 .sg-modal-content img').attr('src', srcImg);
+        //$('html').addClass('flou');
+        //$('#sg1').addClass('flou');
         $('#modal1').show();
     });
 
     $('#modal1').click( function(event) {
         console.log('modal hide');
-        $('html').removeClass('flou');
+        //$('html').removeClass('flou');
+        //$('#sg1').removeClass('flou');
         $('#modal1').hide();
     });
 
