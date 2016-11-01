a5a68f399a6005512c49545b42591fa130fbb2bf
chbes <bes.charles@gmail.com>
2016-11-01 14:22:33 +0100

    modal with background blur

--- js/sg.js
+++ js/sg.js
@@ -4,16 +4,14 @@ $(function() {
         console.log('modal show');
         var srcImg = $(this).attr('src');
         $('#modal1 .sg-modal-content img').attr('src', srcImg);
-        //$('html').addClass('flou');
-        $('html').css('overflow','hidden');
+        $('#main-container').addClass('blur');
         //$('#sg1').addClass('flou');
         $('#modal1').show();
     });
 
     $('#modal1').click( function(event) {
         console.log('modal hide');
-        //$('html').removeClass('flou');
-        $('html').css('overflow','auto');
+        $('#main-container').removeClass('blur');
         //$('#sg1').removeClass('flou');
         $('#modal1').hide();
     });
