e1c02836a37d8c30006531fc5de81101e859e984
chbes <bes.charles@gmail.com>
2016-11-02 17:04:28 +0100

    clean code

--- js/sg.js
+++ js/sg.js
@@ -1,16 +1,18 @@
 $(function() {
 
+    //Open modal when click on image
     $('#sg1 img').click( function() {
-        console.log('modal show');
+        console.log('modal open');
         var srcImg = $(this).attr('src');
         $('#modal1 .sg-modal-content img').attr('src', srcImg);
-        $('#main-container').addClass('blur');
+        $('#main-container').addClass('sg-filter-blur');
         $('#modal1').show();
     });
 
-    $('#modal1').click( function(event) {
-        console.log('modal hide');
-        $('#main-container').removeClass('blur');
+    //Close modal when click on modal
+    $('#modal1').click( function() {
+        console.log('modal close');
+        $('#main-container').removeClass('sg-filter-blur');
         $('#modal1').hide();
     });
 
