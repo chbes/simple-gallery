064d545818712e7beb0aa5d7b96b99bd860da067
chbes <bes.charles@gmail.com>
2016-11-02 16:03:39 +0100

    refactor panel of images

--- js/sg.js
+++ js/sg.js
@@ -5,14 +5,12 @@ $(function() {
         var srcImg = $(this).attr('src');
         $('#modal1 .sg-modal-content img').attr('src', srcImg);
         $('#main-container').addClass('blur');
-        //$('#sg1').addClass('flou');
         $('#modal1').show();
     });
 
     $('#modal1').click( function(event) {
         console.log('modal hide');
         $('#main-container').removeClass('blur');
-        //$('#sg1').removeClass('flou');
         $('#modal1').hide();
     });
 
